7e7596f3c39648ef03970d87e12393fb0d08ea54
auauron <ahronjohn.alera-18@cpu.edu.ph>
2025-09-20 11:49:55 +0800

    feat: killzone

--- riddlescape/scripts/ai_npc.gd
+++ riddlescape/scripts/ai_npc.gd
@@ -1,11 +1,52 @@
-extends CharacterBody2D
+extends Area2D
 
+const speed = 60
+const MOVE_DISTANCE = 48  
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var direction = 1
+var start_position: float
+var left_boundary: float
+var right_boundary: float
 
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	body_entered.connect(_on_body_entered)
+	
+	start_position = position.x
+	left_boundary = start_position - MOVE_DISTANCE
+	right_boundary = start_position + MOVE_DISTANCE
+
 func _process(delta: float) -> void:
-	pass
+	if position.x >= right_boundary:
+		direction = -1
+	elif position.x <= left_boundary:
+		direction = 1
+		#
+	#if ray_cast_right.is_colliding():
+		#direction = -1
+	#if ray_cast_left.is_colliding():
+		#direction = 1
+	#
+	if direction == 1:
+		animated_sprite.flip_h = true  
+	else:
+		animated_sprite.flip_h = false 
+		
+	position.x += direction * speed * delta
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("You failed!")
+
+		var timer = Timer.new()
+		timer.wait_time = 0.3
+		timer.one_shot = true
+		add_child(timer)
+		timer.timeout.connect(_on_death_timer_timeout)
+		timer.start()
+
+func _on_death_timer_timeout():
+	get_tree().reload_current_scene()
--- riddlescape/scripts/player.gd
+++ riddlescape/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-const SPEED = 130.0
+const SPEED = 140.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("jump")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle the movement/deceleration.
